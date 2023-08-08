892d3b9a42dfda152b30ed4624a44c96b83c48f0
vcjuliocesar <vcjuliocesar@outlook.com>
2023-08-08 16:35:04 -0600

    edit:swagger

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
-@app.get('/')
+#change the name app
+app.title = "My first app in FastAPI"
+#change version
+app.version = "0.0.1"
+#tags for group routes
+@app.get('/',tags=['Home'])
 def message():
     return "Hello world"
      
