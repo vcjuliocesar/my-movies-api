14b0dffbc43eebd2127636d4e8053785ec586ee2
vcjuliocesar <vcjuliocesar@outlook.com>
2023-08-09 12:04:20 -0600

    add:pydantic

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI,Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
 
 movies = [
     {
@@ -26,6 +28,15 @@ app.title = "My first app in FastAPI"
 #change version
 app.version = "0.0.1"
 #tags for group routes
+
+class Movie(BaseModel):
+    id:Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating:float
+    category: str 
+    
 @app.get('/',tags=['Home'])
 def message():
     return HTMLResponse("<h1>Hello world</h1>")
@@ -46,26 +57,19 @@ def get_movie_by_category(category:str,year:str):
     return [movie for movie in movies if (movie['category'] == category and movie['year'] == year)]
 
 @app.post('/movies',tags=['Movies'])
-def create_movie(id:int = Body(),title:str = Body(),overview:str = Body(),year:int = Body(),rating:float = Body(),category:str = Body()):
-    movies.append({
-        'id': id,
-        'title': title,
-        'overview': overview,
-        'year': year,
-        'rating':rating,
-        'category': category 
-    })
+def create_movie(movie:Movie):
+    movies.append(movie)
     return movies
 
 @app.put('/movies/{id}',tags=['Movies'])
-def update_movie(id:int,title:str = Body(),overview:str = Body(),year:int = Body(),rating:float = Body(),category:str = Body()):
-    for movie in movies:
-        if movie['id'] == id:
-            movie['title'] = title
-            movie['overview'] = overview
-            movie['year'] = year
-            movie['rating'] = rating
-            movie['category'] = category
+def update_movie(id:int,movie:Movie):
+    for item in movies:
+        if item['id'] == id:
+            item['title'] = movie.title
+            item['overview'] = movie.overview
+            item['year'] = movie.year
+            item['rating'] = movie.rating
+            item['category'] = movie.category
     return movies
 
 @app.delete('/movies/{id}',tags=['Movies'])
