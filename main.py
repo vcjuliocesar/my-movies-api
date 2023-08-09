abf98734a8874b00d0c38b8dcd844c70a07b5fa9
vcjuliocesar <vcjuliocesar@outlook.com>
2023-08-09 17:24:19 -0600

    add:Response Model - Return Type

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI,Body,Path,Query
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse,JSONResponse
 from pydantic import BaseModel,Field
-from typing import Optional
+from typing import Optional,List
 
 movies = [
     {
         'id': 1,
         'title': 'Avatar',
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2009',
+        'year': 2009,
         'rating': 7.8,
         'category': 'Acción'    
     },
@@ -16,7 +16,7 @@ movies = [
         'id': 2,
         'title': 'Avatar2',
         'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
-        'year': '2023',
+        'year': 2023,
         'rating': 7.8,
         'category': 'Acción'    
     } 
@@ -33,7 +33,7 @@ class Movie(BaseModel):
     id:Optional[int] = Field(default=None)
     title:str = Field(min_length=5,max_length=15)
     overview:str = Field(min_length=15,max_length=50)
-    year:int = Field(le=2022)
+    year:int = Field(le=2023)
     rating:float = Field(ge=1,le=10)
     category:str = Field(min_length=5,max_length=15)
     
@@ -42,7 +42,7 @@ class Movie(BaseModel):
             "example":{
                 'id': 1,
                 'title': 'My movie',
-                'overview': "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+                'overview': "En un exuberante",
                 'year': 2023,
                 'rating': 7.8,
                 'category': 'Acción',  
@@ -54,27 +54,27 @@ def message():
     return HTMLResponse("<h1>Hello world</h1>")
 
 #all movies
-@app.get('/movies',tags=['Movies'])
-def get_movies():
-    return movies
+@app.get('/movies',tags=['Movies'],response_model=List[Movie])
+def get_movies() -> List[Movie]:
+    return JSONResponse(content=movies)
 
 #url parameter
-@app.get('/movies/{id}',tags=['Movies'])
-def get_movie(id:int = Path(ge=1,le=2000)):
-    return [movie for movie in movies if movie['id'] == id]
+@app.get('/movies/{id}',tags=['Movies'],response_model=Movie)
+def get_movie(id:int = Path(ge=1,le=2000)) -> Movie:
+    return JSONResponse(content=[movie for movie in movies if movie['id'] == id])
 
 #query parameters
-@app.get('/movies/',tags=['Movies'])
-def get_movie_by_category(category:str = Query(min_length=5,max_length=15),year:str = Query(le=2023)):
-    return [movie for movie in movies if (movie['category'] == category and movie['year'] == year)]
+@app.get('/movies/',tags=['Movies'],response_model=List[Movie])
+def get_movie_by_category(category:str = Query(min_length=5,max_length=15),year:str = Query(le=2023)) -> List[Movie]:
+    return JSONResponse(content=[movie for movie in movies if (movie['category'] == category and movie['year'] == year)])
 
-@app.post('/movies',tags=['Movies'])
-def create_movie(movie:Movie):
+@app.post('/movies',tags=['Movies'],response_model=dict)
+def create_movie(movie:Movie) -> dict:
     movies.append(movie)
-    return movies
+    return JSONResponse(content={"message":"Movie created successfully"})
 
-@app.put('/movies/{id}',tags=['Movies'])
-def update_movie(id:int,movie:Movie):
+@app.put('/movies/{id}',tags=['Movies'],response_model=dict)
+def update_movie(id:int,movie:Movie) -> dict:
     for item in movies:
         if item['id'] == id:
             item['title'] = movie.title
@@ -82,13 +82,16 @@ def update_movie(id:int,movie:Movie):
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-    return movies
+            return JSONResponse(content={"message":"Movie updated successfully"})
+    return JSONResponse(content={"message":"Error"})
 
-@app.delete('/movies/{id}',tags=['Movies'])
-def delete_movie(id:int):
+@app.delete('/movies/{id}',tags=['Movies'],response_model=dict)
+def delete_movie(id:int) -> dict:
     for movie in movies:
     
         if movie['id'] == id:
             movies.remove(movie)
     
-    return movies
+            return JSONResponse(content={"message":"Movie deleted successfully"})
+    
+    return JSONResponse(content={"message":"Error"})
