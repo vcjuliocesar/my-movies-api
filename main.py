e59fe1b2cca01f27750fd9e371342c90a7b4ea32
vcjuliocesar <vcjuliocesar@outlook.com>
2023-08-09 18:04:38 -0600

    add:status code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Body,Path,Query
+from fastapi import FastAPI,Body,Path,Query,status
 from fastapi.responses import HTMLResponse,JSONResponse
 from pydantic import BaseModel,Field
 from typing import Optional,List
@@ -54,26 +54,26 @@ def message():
     return HTMLResponse("<h1>Hello world</h1>")
 
 #all movies
-@app.get('/movies',tags=['Movies'],response_model=List[Movie])
+@app.get('/movies',tags=['Movies'],response_model=List[Movie],status_code=status.HTTP_200_OK)
 def get_movies() -> List[Movie]:
-    return JSONResponse(content=movies)
+    return JSONResponse(status_code=200,content=movies)
 
 #url parameter
-@app.get('/movies/{id}',tags=['Movies'],response_model=Movie)
+@app.get('/movies/{id}',tags=['Movies'],response_model=Movie,status_code=200)
 def get_movie(id:int = Path(ge=1,le=2000)) -> Movie:
-    return JSONResponse(content=[movie for movie in movies if movie['id'] == id])
+    return JSONResponse(status_code=200,content=[movie for movie in movies if movie['id'] == id])
 
 #query parameters
-@app.get('/movies/',tags=['Movies'],response_model=List[Movie])
+@app.get('/movies/',tags=['Movies'],response_model=List[Movie],status_code=200)
 def get_movie_by_category(category:str = Query(min_length=5,max_length=15),year:str = Query(le=2023)) -> List[Movie]:
-    return JSONResponse(content=[movie for movie in movies if (movie['category'] == category and movie['year'] == year)])
+    return JSONResponse(status_code=200,content=[movie for movie in movies if (movie['category'] == category and movie['year'] == year)])
 
-@app.post('/movies',tags=['Movies'],response_model=dict)
+@app.post('/movies',tags=['Movies'],response_model=dict,status_code=201)
 def create_movie(movie:Movie) -> dict:
     movies.append(movie)
-    return JSONResponse(content={"message":"Movie created successfully"})
+    return JSONResponse(status_code=201,content={"message":"Movie created successfully"})
 
-@app.put('/movies/{id}',tags=['Movies'],response_model=dict)
+@app.put('/movies/{id}',tags=['Movies'],response_model=dict,status_code=200)
 def update_movie(id:int,movie:Movie) -> dict:
     for item in movies:
         if item['id'] == id:
@@ -82,16 +82,16 @@ def update_movie(id:int,movie:Movie) -> dict:
             item['year'] = movie.year
             item['rating'] = movie.rating
             item['category'] = movie.category
-            return JSONResponse(content={"message":"Movie updated successfully"})
-    return JSONResponse(content={"message":"Error"})
+            return JSONResponse(status_code=200,content={"message":"Movie updated successfully"})
+    return JSONResponse(status_code=404,content={"message":"Error"})
 
-@app.delete('/movies/{id}',tags=['Movies'],response_model=dict)
+@app.delete('/movies/{id}',tags=['Movies'],response_model=dict,status_code=200)
 def delete_movie(id:int) -> dict:
     for movie in movies:
     
         if movie['id'] == id:
             movies.remove(movie)
     
-            return JSONResponse(content={"message":"Movie deleted successfully"})
+            return JSONResponse(status_code=200,content={"message":"Movie deleted successfully"})
     
-    return JSONResponse(content={"message":"Error"})
+    return JSONResponse(status_code=404,content={"message":"Error"})
